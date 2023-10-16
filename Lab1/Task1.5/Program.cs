fafd5e18ac76a68f5bc2a963c34c0c3176ac2508
HGB3009 <22520103@gm.uit.edu.vn>
2023-10-16 23:25:27 +0700

    Bao add

--- Lab1/Task1.5/Program.cs
+++ Lab1/Task1.5/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.Write("Nhap duong dan cua thu muc: ");
+            string folderPath = Console.ReadLine();
+
+            if (Directory.Exists(folderPath))
+            {
+                Console.WriteLine("Danh sach cac tep tin trong thu muc:");
+                string[] files = Directory.GetFiles(folderPath);
+                foreach (string file in files)
+                {
+                    Console.WriteLine(Path.GetFileName(file));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Thu muc khong ton tai.");
+            }
+        }
+    }
+}
