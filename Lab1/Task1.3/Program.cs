602a9d2f8ca86dfdcf760ab620c3b039ee9a2243
PhanVanDiej <22520182@gm.uit.edu.vn>
2023-10-17 21:54:28 +0700

    VanDai add 1.3 and 1.6

---
+++ Lab1/Task1.3/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using Microsoft.VisualBasic;
+
+namespace Myproject_Lab1
+{
+    class CustomDataType
+    {
+        public int[] array = new int[100];
+        public string[] strings = new string[100];
+        public float[] floats = new float[100];
+        public int type;
+        public int Max(int[] array)
+        {
+            int maxValue = -Int32.MaxValue;
+            for (int i = 0; i < array.Length; ++i)
+            {
+                maxValue = Math.Max(maxValue, array[i]);
+            }
+            return maxValue;
+        }
+        public float Max(float[] floats)
+        {
+            float maxValue = -float.MaxValue;
+            for (int i = 0; i < floats.Length; ++i)
+                maxValue = Math.Max(maxValue, floats[i]);
+            return maxValue;
+        }
+        public string Max(string[] strings)
+        {
+            string maxValue = "";
+            for (int i = 0; i < strings.Length; ++i)
+                if (maxValue.Length < strings[i].Length)
+                    maxValue = strings[i];
+            return maxValue;
+        }
+        public int Min(int[] array)
+        {
+            int minValue = Int32.MaxValue;
+            for (int i = 0; i < array.Length; ++i)
+            {
+                minValue = Math.Min(minValue, array[i]);
+            }
+            return minValue;
+        }
+        public float Min(float[] floats)
+        {
+            float minValue = -float.MinValue;
+            for (int i = 0; i < floats.Length; ++i)
+                minValue = Math.Min(minValue, floats[i]);
+            return minValue;
+        }
+        public string Min(string[] strings)
+        {
+            string minValue = "";
+            for (int i = 0; i < strings.Length; ++i)
+                if (minValue.Length < strings[i].Length)
+                    minValue = strings[i];
+            return minValue;
+        }
+        public void Input(int n, string[] temp)
+        {
+            if (type == 1)
+                for (int i = 0; i < n; ++i)
+                    array[i] = int.Parse(temp[i]);
+            else if (type == 2)
+                for (int i = 0; i < n; ++i)
+                    floats[i] = float.Parse(temp[i]);
+            else
+                strings = temp;
+        }
+    }
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Find_Max_Min();
+        }
+        static public void Find_Max_Min()
+        {
+            CustomDataType A = new CustomDataType();
+
+            Console.WriteLine("*/ 1. Mang kieu so nguyen");
+            Console.WriteLine("  2. Mang kieu so thuc");
+            Console.WriteLine("  3. Mang kieu string /*");
+
+            Console.Write("Chon kieu du lieu: ");
+            string readData = Console.ReadLine();
+
+            A.type = int.Parse(readData);
+
+            Console.Write("So luong phan tu: ");
+            readData = Console.ReadLine();
+
+            int n = int.Parse(readData);
+
+            Console.Write("Nhap " + n + " phan tu: ");
+            string data = Console.ReadLine();
+
+            string[] temp = data.Split(" ");
+            A.Input(n, temp);
+
+            Console.Write("1 de tim Max, 2 de tim Min: ");
+            readData = Console.ReadLine();
+
+            int request = int.Parse(readData);
+            if (request == 1)
+            {
+                if (A.type == 1)
+                    Console.WriteLine(A.Max(A.array));
+                else if (A.type == 2)
+                    Console.WriteLine(A.Max(A.floats));
+                else
+                    Console.WriteLine(A.Max(A.strings));
+            }
+            else
+            {
+                if (A.type == 1)
+                    Console.WriteLine(A.Min(A.array));
+                else if (A.type == 2)
+                    Console.WriteLine(A.Min(A.floats));
+                else
+                    Console.WriteLine(A.Min(A.strings));
+            }
+        }
+    }
+}
---
+++ Lab1/Task1.6/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Task1_6
+{
+    interface IThinking
+    {
+        void thinking_behavior();
+    }
+    interface IIntelligent
+    {
+        void intelligent_behavior();
+    }
+    interface IAbility : IThinking, IIntelligent
+    {
+
+    }
+    public class Mamal
+    {
+        public string characteristics;
+        public Mamal(string characteristics)
+        {
+            this.characteristics = characteristics;
+        }
+    }
+    public class Whale : Mamal
+    {
+        public Whale(string name) : base(name)
+        {
+
+        }
+    }
+    public class Human : Mamal, IAbility
+    {
+        public Human(string name) : base(name)
+        {
+
+        }
+        public void thinking_behavior()
+        {
+            Console.WriteLine("Think about their life");
+        }
+        public void intelligent_behavior()
+        {
+            Console.WriteLine("Know the 2+2 Math");
+        }
+    }
+    class Programs
+    {
+        private static void Main(string[] args)
+        {
+            Human a = new Human("Phan Van Dai");
+            a.thinking_behavior();
+            a.intelligent_behavior();
+        }
+
+    }
+
+}
