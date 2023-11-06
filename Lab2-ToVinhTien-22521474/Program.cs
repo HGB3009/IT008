3ef40a7d5a71790fc4b4a4f005ed4eb067541764
Terry <138951865+Terry-UIT@users.noreply.github.com>
2023-11-06 23:06:36 +0700

    Updated by terry

---
+++ Lab2-ToVinhTien-22521474/Program.cs
@@ -0,0 +1,16 @@
+namespace Lab2_ToVinhTien_22521474
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
